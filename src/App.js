a99e75d8b48dfe9deef108107e0456da03ba0d53
moidryg <nazar.kryzhanivsky@gmail.com>
2022-06-03 16:05:21 +0300

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import './scss/app.scss';
+import PizzaBlock from './components/PizzaBlock';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title="Pizza Cool" price="499" />
+            <PizzaBlock title="Pizza Nice" price="699" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
